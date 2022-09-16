b66e53adddf7b91ca1615b7e045ea2590de82311
venkiteshsa <venkitesh.sa@inapp.com>
2022-09-16 08:39:51 +0530

    Add project files.

---
+++ ERP-cart/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ERP_cart;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ERP-cart/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ERP_cart;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
